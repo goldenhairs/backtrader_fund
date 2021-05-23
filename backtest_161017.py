92409dbf17de6c5f43a15493817102523768669f
Zucchiniy <banshiliuli1990@sina.com>
2021-05-24 00:20:07 +0800

    :recycle: 重构项目中引入方法和类的内容
    修改：     TODOS.org
    修改：     backtest_161017.py
    修改：     backtest_513050.py
    修改：     backtest_momOsc.py
    删除：     backtest_week.py

--- backtest_161017.py
+++ backtest_161017.py
@@ -1,69 +1,39 @@
 import datetime
+from backtest.backtest_feeds import OpenFundCsvData, ETFCsvData
+from backtest.backtest_strategy import DeclineStrategy
 
 import backtrader as bt
-
 import backtrader.analyzers as btanalyzers
-
-
-class strategy(bt.Strategy):
-    """
-    实现富国中证 500 定投，如果跌幅达到 2% 则买入
-    """
-
-    def log(self, txt, dt=None):
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
-
-    def __init__(self):
-        self.dataprice = self.datas[0].close
-        self.rate = self.datas[0].volume
-        self.all_size = 0
-        self.all_fund_menoy = 0
-
-    def next(self):
-        if self.rate[0] <= -2.0 or self.rate[0] + self.rate[-1] <= -2.0:
-            buysize = round(1000 / self.dataprice[0], 2)
-            self.buy(size=buysize)
 
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
-    # start_date = datetime.datetime(2019, 5, 14)
     start_date = datetime.datetime(2020, 7, 23)
-    # end_date = datetime.datetime(2020, 1, 7)
     end_date = datetime.datetime(2021, 5, 20)
 
-    data = bt.feeds.GenericCSVData(dataname=r'datas/161017.csv',
-                                   fromdate=start_date,
-                                   todate=end_date,
-                                   nullvalue=0.0,
-                                   dtformat=('%Y-%m-%d'),
-                                   datetime=0,
-                                   open=-1,
-                                   high=-1,
-                                   low=-1,
-                                   close=1,
-                                   volume=2,
-                                   openinterest=-1)
+    data = OpenFundCsvData(dataname=r'datas/161017.csv',
+                           fromdate=start_date,
+                           todate=end_date,)
 
-    cerebro.adddata(data)
-    cerebro.addstrategy(strategy)
+    data1 = ETFCsvData(dataname=r'datas/sh510500.csv', fromdate=start_date, todate=end_date)
+    cerebro.adddata(data1)
+    cerebro.addstrategy(DeclineStrategy)
 
     cash = 100000
     cerebro.broker.setcash(cash)
     cerebro.broker.setcommission(commission=0.00015)
 
-    cerebro.addanalyzer(btanalyzers.SharpeRatio_A, _name='sharp_a')
-    cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='sharp')
+    cerebro.addanalyzer(btanalyzers.SharpeRatio_A, _name='sharp')
     cerebro.addanalyzer(btanalyzers.AnnualReturn, _name='annualreturn')
-    # cerebro.addanalyzer(btanalyzers.DrawDown, _name='drawdown')
-    strats = cerebro.run()
 
+    strats = cerebro.run()
     strat = strats[0]
-    print(cerebro.broker.get_value())
-    print(cerebro.broker.get_cash())
+
+    return_all = cerebro.broker.get_value() - cash
+    used_cash = cash - cerebro.broker.get_cash()
+    # roi = round(return_all/used_cash, 2) * 100
     print('夏普率:', strat.analyzers.sharp.get_analysis())
-    print('夏普率a:', strat.analyzers.sharp_a.get_analysis())
     print('年化回报:', strat.analyzers.annualreturn.get_analysis())
-    # print('最大回撤:', strat.analyzers.drawdown.get_analysis())
-    cerebro.plot(volume=False)
+    # print(f'总收益: {roi}%')
+
+    cerebro.plot()
--- backtest_513050.py
+++ backtest_513050.py
@@ -1,62 +1,38 @@
-import calendar
 import datetime
 
 import backtrader as bt
+from backtest.backtest_feeds import ETFCsvData
+from backtest.backtest_strategy import WeekStrategy
 
-
-def weekday(date):
-    date_time = date
-    return (calendar.weekday(date_time.year, date_time.month, date_time.day))
-
-
-class strategy(bt.Strategy):
-    params = (('maperiod', 20), )
-
-    def log(self, txt, dt=None):
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
-
-    def __init__(self):
-        self.dataprice = self.datas[0].close
-        self.all_size = 0
-        self.all_fund_menoy = 0
-
-    def next(self):
-        if weekday(self.datas[0].datetime.date(0)) == 3:
-            buy_size = round(250 / self.dataprice[0], 2)
-            self.all_fund_menoy += 250
-            self.buy(size=buy_size)
-
+import backtrader.analyzers as btanalyzers
 
 if __name__ == '__main__':
-    import backtrader.analyzers as btanalyzers
     cerebro = bt.Cerebro()
     start_date = datetime.datetime(2018, 1, 1)
     end_date = datetime.datetime(2021, 5, 13)
 
-    data = bt.feeds.GenericCSVData(dataname=r'datas/sh513050.csv',
-                                   fromdate=start_date,
-                                   todate=end_date,
-                                   nullvalue=0.0,
-                                   dtformat=('%Y-%m-%d'),
-                                   datetime=1,
-                                   open=2,
-                                   high=3,
-                                   low=4,
-                                   close=5,
-                                   volume=6,
-                                   openinterest=-1)
+    data = ETFCsvData(dataname=r'datas/sh513050.csv',
+                      fromdate=start_date,
+                      todate=end_date,)
 
     cerebro.adddata(data)
-    cerebro.addstrategy(strategy)
+    cerebro.addstrategy(WeekStrategy)
 
     cash = 100000
     cerebro.broker.setcash(cash)
     cerebro.broker.setcommission(commission=0.00015)
+
     cerebro.addanalyzer(btanalyzers.SharpeRatio_A, _name='sharp')
     cerebro.addanalyzer(btanalyzers.AnnualReturn, _name='annualreturn')
 
     strats = cerebro.run()
     strat = strats[0]
+
+    return_all = cerebro.broker.get_value() - cash
+    used_cash = cash - cerebro.broker.get_cash()
+    roi = round(return_all/used_cash, 2) * 100
     print('夏普率:', strat.analyzers.sharp.get_analysis())
     print('年化回报:', strat.analyzers.annualreturn.get_analysis())
+    print(f'总收益: {roi}%')
+
+    cerebro.plot()
