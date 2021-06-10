8395ef97a7854651c5b422baee0a43296887ead4
Zucchiniy <banshiliuli1990@sina.com>
2021-06-10 17:50:38 +0800

    sparkles: 新增基础策略，增加 log/notify_order/notify_trade 方法
    修改：     backtest/backtest_strategy.py

--- backtest/backtest_strategy.py
+++ backtest/backtest_strategy.py
@@ -1,5 +1,50 @@
-import backtrader as bt
 import calendar
+
+import backtrader as bt
+
+
+class MyStrategy(bt.Strategy):
+    params = (('printlog', False), )
+
+    def log(self, txt, dt=None, doprint=False):
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        pass
+
+    def next(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    f'执行买入，价格：{order.executed.price:.2f}，花费：{order.executed.value:.2f}，手续费：{order.executed.comm:.2f}'
+                )
+
+            else:
+                self.log(
+                    f'执行卖出，价格：{order.executed.price:.2f}，花费：{order.executed.value:.2f}，手续费：{order.executed.comm:.2f}'
+                )
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('交易取消、保证金不足、交易被拒绝')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log(f'营业利润，毛利润：{trade.pnl:.2f}，净利润：{trade.pnlcomm:.2f}')
+
 
 """
 实现富国中证 500 定投，如果跌幅达到 2% 则买入
@@ -7,9 +52,7 @@ import calendar
 
 
 class DeclineStrategy(bt.Strategy):
-    params = (
-        ('decline', -2.0),
-    )
+    params = (('decline', -2.0), )
 
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
@@ -17,11 +60,13 @@ class DeclineStrategy(bt.Strategy):
 
     def __init__(self):
         self.dataprice = self.datas[0].close
-        self.rate = (self.datas[0].close - self.datas[0].open) / self.datas[0].open * 100
+        self.rate = (self.datas[0].close -
+                     self.datas[0].open) / self.datas[0].open * 100
 
     def next(self):
         print(self.rate[0])
-        if self.rate[0] <= self.params.decline or (self.rate[0] + self.rate[-1]) <= self.params.decline:
+        if self.rate[0] <= self.params.decline or (
+                self.rate[0] + self.rate[-1]) <= self.params.decline:
             buy_size = round(250 / self.dataprice[0], 2)
             print(buy_size)
             self.buy(size=100)
@@ -33,11 +78,12 @@ class DeclineStrategy(bt.Strategy):
 
 
 class WeekStrategy(bt.Strategy):
-    params = (('weeknum', 3),)
+    params = (('weeknum', 3), )
 
     def weekday(self, date):
         date_time = date
-        return (calendar.weekday(date_time.year, date_time.month, date_time.day))
+        return (calendar.weekday(date_time.year, date_time.month,
+                                 date_time.day))
 
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
@@ -57,21 +103,26 @@ class WeekStrategy(bt.Strategy):
 """
 
 
-class MomOscStrategy(bt.Strategy):
+class MomOscStrategy(MyStrategy):
     params = (
         ('period', 20),
+        ('printlog', True),
     )
 
     def __init__(self):
         self.dataprice = self.datas[0].close
         self.order = None
         self.month = -1
-        self.mom = [bt.indicators.MomentumOscillator(i, period=self.params.period) for i in self.datas]
+        self.mom = [
+            bt.indicators.MomentumOscillator(i, period=self.params.period)
+            for i in self.datas
+        ]
 
     def next(self):
         buy_id = 0
 
         c = [i.momosc[0] for i in self.mom]
+        [self.log(i) for i in c]
         index, value = c.index(max(c)), max(c)
 
         if value > 100:
@@ -79,7 +130,8 @@ class MomOscStrategy(bt.Strategy):
 
         for i in range(0, len(c)):
             if i != buy_id:
-                position_size = self.broker.getposition(data=self.datas[i]).size
+                position_size = self.broker.getposition(
+                    data=self.datas[i]).size
                 if position_size != 0:
                     self.order_target_percent(data=self.datas[i], target=0)
 
@@ -89,22 +141,22 @@ class MomOscStrategy(bt.Strategy):
 
     def stop(self):
         return_all = self.broker.getvalue() / 200000.0
-        print('{0}, {1}%, {2}%'.format(self.params.period,
-                                       round((return_all - 1.0) * 100, 2),
-                                       round((pow(return_all, 1.0 / 8) - 1.0) * 100, 2)
-                                       ))
+        print('{0}, {1}%, {2}%'.format(
+            self.params.period, round((return_all - 1.0) * 100, 2),
+            round((pow(return_all, 1.0 / 8) - 1.0) * 100, 2)))
 
 
 class MomStrategy(bt.Strategy):
-    params = (
-        ('period', 20),
-    )
+    data = ('period', 20),
 
     def __init__(self):
         self.dataprice = self.datas[0].close
         self.order = None
         self.month = -1
-        self.mom = [bt.indicators.Momentum(i, period=self.params.period) for i in self.datas]
+        self.mom = [
+            bt.indicators.Momentum(i, period=self.params.period)
+            for i in self.datas
+        ]
 
     def next(self):
         buy_id = 0
@@ -117,7 +169,8 @@ class MomStrategy(bt.Strategy):
 
         for i in range(0, len(c)):
             if i != buy_id:
-                position_size = self.broker.getposition(data=self.datas[i]).size
+                position_size = self.broker.getposition(
+                    data=self.datas[i]).size
                 if position_size != 0:
                     self.order_target_percent(data=self.datas[i], target=0)
 
@@ -127,22 +180,22 @@ class MomStrategy(bt.Strategy):
 
     def stop(self):
         return_all = self.broker.getvalue() / 200000.0
-        print('{0}, {1}%, {2}%'.format(self.params.period,
-                                       round((return_all - 1.0) * 100, 2),
-                                       round((pow(return_all, 1.0 / 8) - 1.0) * 100, 2)
-                                       ))
+        print('{0}, {1}%, {2}%'.format(
+            self.params.period, round((return_all - 1.0) * 100, 2),
+            round((pow(return_all, 1.0 / 8) - 1.0) * 100, 2)))
 
 
 class BBandStrategy(bt.Strategy):
-    params = (
-        ('period', 20),
-    )
+    params = (('period', 20), )
 
     def __init__(self):
         self.dataprice = self.datas[0].close
         self.order = None
         self.month = -1
-        self.bbandPcts = [bt.indicators.BollingerBandsPct(i, period=self.params.period) for i in self.datas]
+        self.bbandPcts = [
+            bt.indicators.BollingerBandsPct(i, period=self.params.period)
+            for i in self.datas
+        ]
 
     def next(self):
         buy_id = 0
@@ -155,7 +208,8 @@ class BBandStrategy(bt.Strategy):
 
         for i in range(0, len(c)):
             if i != buy_id:
-                position_size = self.broker.getposition(data=self.datas[i]).size
+                position_size = self.broker.getposition(
+                    data=self.datas[i]).size
                 if position_size != 0:
                     self.order_target_percent(data=self.datas[i], target=0)
 
@@ -165,7 +219,6 @@ class BBandStrategy(bt.Strategy):
 
     def stop(self):
         return_all = self.broker.getvalue() / 200000.0
-        print('{0}, {1}%, {2}%'.format(self.params.period,
-                                       round((return_all - 1.0) * 100, 2),
-                                       round((pow(return_all, 1.0 / 8) - 1.0) * 100, 2)
-                                       ))
+        print('{0}, {1}%, {2}%'.format(
+            self.params.period, round((return_all - 1.0) * 100, 2),
+            round((pow(return_all, 1.0 / 8) - 1.0) * 100, 2)))
