74a741f0f61cf705c19e08a26a62c6dcf7558651
Zucchiniy <banshiliuli1990@sina.com>
2021-06-10 17:50:07 +0800

    :bug: 将结束时间统一成当日
    修改：     backtest_run.py

--- backtest_run.py
+++ backtest_run.py
@@ -1,9 +1,9 @@
 import datetime
 
-from backtest.backtest_strategy import BBandStrategy, MomOscStrategy, MomStrategy
+from backtest.backtest_cerebro import backtestopt, backtestrun
+from backtest.backtest_strategy import (BBandStrategy, MomOscStrategy,
+                                        MomStrategy)
 
-from backtest.backtest_cerebro import backtestrun, backtestopt
-import pytest
 
 def setup_data():
     """
@@ -30,7 +30,7 @@ def setup_data():
     4, 18, 179.22%, 13.7%
     """
     hs300zz500 = ['sh510310', 'sh510500']
-    
+
     funds_1 = ['sz159915'] + hs300zz500
     funds_2 = ['sz159952'] + hs300zz500
 
@@ -39,58 +39,99 @@ def setup_data():
     funds_3 = ['sz159915'] + gold_nas
     funds_4 = ['sz159952'] + gold_nas
 
-    return funds_1,funds_2, funds_3, funds_4
+    res = {
+        'funds_1': funds_1,
+        'funds_2': funds_2,
+        'funds_3': funds_3,
+        'funds_4': funds_4
+    }
+
+    return res
 
 
-def test_momoscstrategy():
+def test_momoscstrategy(optflag=False, fund_name='funds_1'):
     datas = setup_data()
     cash = 200000.00
     periods = range(1, 60)
 
     opt_start_date = datetime.datetime(2017, 5, 28)
-    opt_end_date = datetime.datetime(2021, 5, 28)
 
     run_start_date = datetime.datetime(2020, 1, 6)
-    run_end_date = datetime.datetime.now()
+    end_date = datetime.datetime.now()
 
-    # backtestopt(cash=cash, funds=datas[3], periods=periods, start_date=opt_start_date,end_date=opt_end_date, strategy=MomOscStrategy)
+    if optflag:
+        backtestopt(cash=cash,
+                    funds=datas[fund_name],
+                    periods=periods,
+                    start_date=opt_start_date,
+                    end_date=end_date,
+                    strategy=MomOscStrategy)
+    else:
+        backtestrun(cash=cash,
+                    funds=datas[fund_name],
+                    period=18,
+                    start_date=run_start_date,
+                    end_date=end_date,
+                    strategy=MomOscStrategy)
 
 
-    backtestrun(cash=cash, funds=datas[3], period=18, start_date=run_start_date, end_date=run_end_date, strategy=MomOscStrategy)
-
-
-def test_momstrategy():
+def test_momstrategy(optflag=False, funds_name='funds_1'):
     datas = setup_data()
     cash = 200000.00
     periods = range(1, 60)
 
     opt_start_date = datetime.datetime(2017, 5, 28)
-    opt_end_date = datetime.datetime(2021, 5, 28)
+    opt_end_date = datetime.datetime.now()
 
     run_start_date = datetime.datetime(2020, 1, 6)
     run_end_date = datetime.datetime.now()
 
-    # backtestopt(cash=cash, funds=datas[3], periods=periods, start_date=opt_start_date,end_date=opt_end_date, strategy=MomStrategy)
+    if optflag:
+        backtestopt(cash=cash,
+                    funds=datas[funds_name],
+                    periods=periods,
+                    start_date=opt_start_date,
+                    end_date=opt_end_date,
+                    strategy=MomStrategy)
+    else:
+        backtestrun(cash=cash,
+                    funds=datas[funds_name],
+                    period=18,
+                    start_date=run_start_date,
+                    end_date=run_end_date,
+                    strategy=MomStrategy)
 
 
-    backtestrun(cash=cash, funds=datas[3], period=18, start_date=run_start_date, end_date=run_end_date, strategy=MomStrategy)
-
-
-def test_bbandstrategy():
+def test_bbandstrategy(optflag=False, fund_name='funds_1'):
     datas = setup_data()
     cash = 200000.00
     periods = range(1, 60)
 
     opt_start_date = datetime.datetime(2017, 5, 28)
-    opt_end_date = datetime.datetime(2021, 5, 28)
-
     run_start_date = datetime.datetime(2020, 1, 6)
-    run_end_date = datetime.datetime.now()
+    end_date = datetime.datetime.now()
 
-    # backtestopt(cash=cash, funds=datas[3], periods=periods, start_date=opt_start_date,end_date=opt_end_date, strategy=BBandStrategy)
+    if optflag:
+        backtestopt(cash=cash,
+                    funds=datas[fund_name],
+                    periods=periods,
+                    start_date=opt_start_date,
+                    end_date=end_date,
+                    strategy=BBandStrategy)
+    else:
+        backtestrun(cash=cash,
+                    funds=datas[fund_name],
+                    period=18,
+                    start_date=run_start_date,
+                    end_date=end_date,
+                    strategy=BBandStrategy)
 
-
-    backtestrun(cash=cash, funds=datas[3], period=18, start_date=run_start_date, end_date=run_end_date, strategy=BBandStrategy)
 
 if __name__ == '__main__':
-    pytest.main(['-s', 'backtest_momOsc.py'])
+    """
+    1,16, 163.01%, 12.85%
+    2,16, 156.6%, 12.5%
+    3,18, 202.09%, 14.82%
+    4,18, 178.64%, 13.67%
+    """
+    test_momoscstrategy(fund_name='funds_3')
